7730684c733fa2cd84d77289b738eb56a5caad74
Thanathorn <Thanathorn_tee@cmu.ac.th>
2025-12-04 19:08:16 +0700

    clean initial commit

---
+++ lib/main.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smp Shop',
+      theme: ThemeData(
+        colorScheme: .fromSeed(
+          seedColor: const Color.fromARGB(255, 11, 238, 200),
+        ),
+      ),
+      home: const MyHomePage(title: 'Calculate Change')
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var price = TextEditingController();
+  var amount = TextEditingController();
+  var change = TextEditingController();
+  var getMoney = TextEditingController();
+
+  double _total = 0;
+  double _change = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column( 
+
+          children: [
+           Text(
+  "Calculate Change",
+  style: TextStyle(
+    fontFamily: "maa",
+    fontSize: 48,
+    fontWeight: FontWeight.w700,
+    fontStyle: FontStyle.italic,
+    color: Colors.blue,
+    backgroundColor: Colors.greenAccent,
+  ),
+),
+Image.asset("assets/burger.png",height: 100),
+SizedBox(height:20),
+Image.network(
+  "https://d11a6trkgmumsb.cloudfront.net/original/3X/6/5/65137061474887dfa0f183f2bc118a3e52fc353e.gif",height:60),
+       
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: priceTextField(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: amountTextField(),
+            ),
+            calculateButton(),
+            showTotalText(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: receiveMoneyTextField(),
+            ),
+            changeCalculateBotton(),
+            showChangeText(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget priceTextField() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: price,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: "price per item",
+        ),
+        keyboardType: TextInputType.numberWithOptions(),
+      ),
+    );
+  }
+
+  Widget amountTextField() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: amount,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: "amount of item",
+        ),
+        keyboardType: TextInputType.numberWithOptions(),
+      ),
+    );
+  }
+
+  Widget calculateButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (price.text.isNotEmpty && amount.text.isNotEmpty) {
+          setState(() {
+            _total = double.parse(price.text) * double.parse(amount.text);
+          });
+        }
+      },
+      child: Text("Calculate Total "),
+    );
+  }
+
+  Widget showTotalText() {
+    return Text("Total: $_total Baht");
+  }
+
+  Widget receiveMoneyTextField() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: getMoney,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: "get money",
+        ),
+        keyboardType: TextInputType.numberWithOptions(),
+      ),
+    );
+  }
+
+  Widget changeCalculateBotton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (getMoney.text.isNotEmpty) {
+          double _money = double.parse(getMoney.text);
+
+          if (_money < _total) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text("money is not enough")));
+          }
+          setState(() {
+            _change = _money - _total;
+          });
+        }
+      },
+      child: Text("Change Calculate"),
+    );
+  }
+
+  Widget showChangeText() {
+    return Text("Change: $_change Baht");
+  }
+}
