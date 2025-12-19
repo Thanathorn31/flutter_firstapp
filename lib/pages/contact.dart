7f7b47715c1b26b02c9fcc59f80f644332fe9a8e
Thanathorn <Thanathorn_tee@cmu.ac.th>
2025-12-19 09:53:14 +0700

    add list tile on contact page

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -8,12 +8,33 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+  List mydata = ['apple', 'banana', 'papaya'];
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
         Center(
-          child: Column(children: [Text("Thanathorn"), Text("@CAMT: CMU")]),
+          child: Column(
+            children: [
+              ListTile(
+                onTap: () {},
+                leading: FlutterLogo(),
+                title: Text(mydata[0]),
+                subtitle: Text("subtitle"),
+                trailing: FlutterLogo(),
+              ),
+              ListTile(
+                onTap: () {},
+                leading: FlutterLogo(),
+                title: Text(mydata[1]),
+              ),
+              ListTile(
+                onTap: () {},
+                leading: FlutterLogo(),
+                title: Text(mydata[2]),
+              ),
+            ],
+          ),
         ),
       ],
     );
